4ab93e679e8a23ea22950b2966e24d51addc16d6
Kerim <kkerimow123@gmail.com>
2023-10-17 07:40:19 +0300

    homework

--- practice_44h/js/script.js
+++ practice_44h/js/script.js
@@ -8,7 +8,7 @@
 Реализовать только при помощи JS
 
 4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
+Отсортировать их по алфавиту
 
 5) Добавить нумерацию выведенных фильмов */
 
@@ -24,3 +24,31 @@ const movieDB = {
     ]
 };
 
+
+const adv = document.querySelectorAll('.promo__adv img'),
+      poster = document.querySelector('.promo__bg'),
+      genre = poster.querySelector('.promo__genre'),
+      movieList = document.querySelector('.promo__interactive-list');
+
+// 1
+adv.forEach(item => {
+    item.remove();
+});
+
+// 2
+genre.textContent = 'Драма';
+
+// 3
+poster.style.backgroundImage = 'url(img/bg.jpg)';
+
+// 4
+movieList.innerHTML = '';
+
+movieDB.movies.sort();
+
+movieDB.movies.forEach((film, i) => {
+    movieList.innerHTML += `
+        <li class="promo__interactive-item">${i+1} ${film}
+            <div class="delete"></div>
+        </li>`
+})
