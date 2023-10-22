aab3dacc45a832cd21b6566bf6470bf90c1ca76b
Kerim <kkerimow123@gmail.com>
2023-10-22 15:08:37 +0300

    learn about Map and Set

--- practice_55/script.js
+++ practice_55/script.js
@@ -55,7 +55,7 @@
 // console.log(myAwesomeDB);
 
 
-// ------------------- Дескриа=пторв свойств -------------------------
+// ------------------- Дескрипторы свойств -------------------------
 
 // writable - если этот параметр true то свойство в обекте можно будеть изменит (false только для чтение)
 // enumerable - если этот параметр true то свойство будеть перечислятся в цикл в false цикл будеть игнорироват
@@ -64,13 +64,13 @@
 
 // Object.defineProperty - с помощю defineProperty мы можем создать свойству и дескрипторы там будет по умолчанию false
 
-const user = {
-    name: 'Alex',
-    surname: 'Smith',
-    showMyPublicData: function() {
-        console.log(`${this.name} ${this.surname}`);
-    }
-}
+// const user = {
+//     name: 'Alex',
+//     surname: 'Smith',
+//     showMyPublicData: function() {
+//         console.log(`${this.name} ${this.surname}`);
+//     }
+// }
 // Object.defineProperty(user, 'name', {writable: false});
 // Object.defineProperty(user, 'gender', {value: 'male'});
 
@@ -87,8 +87,238 @@ const user = {
 // Object.defineProperty(user, 'showMyPublicData', {enumerable: false})
 // for (const key in user) console.log(key);
 
-Object.defineProperties(user, {
-    name: {writable: false},
-    surname: {writable: false}
-})
+// Object.defineProperties(user, {
+//     name: {writable: false},
+//     surname: {writable: false}
+// })
 
+
+
+// ------------------- Итерируемые конструкции -------------------------
+
+
+// const user = {
+//     name: 'Alex',
+//     surname: 'Smith',
+//     birthday: '20/04/1993',
+//     showMyPublicData: function() {
+//         console.log(`${this.name} ${this.surname}`);
+//     }
+// }
+
+// for (const key in user) {
+//     console.log(user[key]);
+// }
+
+// const arr = ['a', 'b', 'c'];
+
+// const salaries = {
+//     john: 500,
+//     ivan: 1000,
+//     ann: 5000,
+//     sayHello: function() {
+//         console.log('Hello');
+//     }
+// };
+
+// salaries[Symbol.iterator] = function() {
+//     return {
+//         current: this.john,
+//         last: this.ann,
+//         next() {
+//             if (this.current < this.last) {
+//                 this.current += 500;
+//                 return {done: false, value: this.current}
+//             }else {
+//                 return {done: true}
+//             }
+//         }
+//     }
+// }
+
+// for (const res of salaries) {
+//     console.log(res);
+// }
+// console.log(Object.keys(salaries).length);
+// когда цикл forof будеть запускатся на обекте salaries то он вызывает этот метод и этот метод
+// должен нам вернут итератор это объект с методом next дальше forof будеть работать с этим
+// объектам которое вернулся из этого метода. Когда forof получил объект с методом next
+// и наш перебор (итератор) хочет получит следуюшее значение он вызывает метод next с этого объекта
+// и результать вызова метода next должен иметь определенный вид
+
+
+
+
+
+
+
+// ------------------- Map -------------------------
+
+
+// const user = {
+//     name: 'Alex',
+//     surname: 'Smith',
+//     birthday: '20/04/1993',
+//     showMyPublicData: function() {
+//         console.log(`${this.name} ${this.surname}`);
+//     }
+// };
+
+// console.log(typeof Object.keys(user)[0]);
+
+// const shops = [
+//     {rice: 500},
+//     {oil: 200},
+//     {bread: 50}
+// ];
+
+// const map = new Map([
+//     [{paper: 400}, 12000]
+// ]);
+
+// const budget = [5000, 15000, 25000];
+
+// map.set(shops[0], 5000)
+//    .set(shops[1], 8000)
+//    .set(shops[2], 25000);
+// shops.forEach((shop, i) => {
+//     map.set(shop, budget[i]);
+// })
+
+// console.log(map);
+
+// console.log(map.get(shops[0]));
+// console.log(map.has(shops[0]));
+// console.log(map.delete(shops[0]));
+// console.log(map.clear(shops[0]));
+// console.log(map.size);
+
+// console.log(map.keys());
+
+// const goods = [];
+// for (const shop of map.keys()) {
+//     goods.push(Object.keys(shop)[0]);
+// }
+// console.log(goods);
+
+// for (const price of map.values()) {
+//     console.log(price);
+// }
+
+// for (const item of map.entries()) {
+//     console.log(item[0], item[1]);
+// }
+
+// map.forEach((value, key, map) => {
+//     console.log(key, value);
+// });
+
+// const user = {
+//     name: 'Alex',
+//     surname: 'Smith',
+//     birthday: '20/04/1993',
+//     showMyPublicData: function() {
+//         console.log(`${this.name} ${this.surname}`);
+//     }
+// };
+
+// const userMap = new Map(Object.entries(user));
+// console.log(userMap);
+
+// const user = {
+//     name: 'Kerim',
+//     surname: 'Kerimov',
+//     [Symbol('id')]: 456
+// };
+
+// let id = Symbol('id');
+// user[id] = 123
+// console.log(typeof user[id]);
+// console.log(typeof id);
+
+// console.log(Object.keys(user).length);
+
+
+
+
+// ------------------- Set -------------------------
+
+// const arr = [1, 1, 2, 2, 4, 5, 6, 5];
+// const arr = ['Alex', 'Ann', 'Oleg', 'Alex'];
+
+// const set = new Set(arr);
+
+// set.add('Ivan')
+//    .add('Oleg');
+
+// console.log(set);
+
+// // for (const value of set) console.log(value);
+// // console.log(set[1]);
+
+// set.forEach((value, valueAgaing, set) => {
+//     console.log(value, valueAgaing);
+// })
+
+
+
+
+// ------------------- BigInt -------------------------
+
+// console.log(15n / 3n);
+
+
+
+
+// function amountOfPages(summary){
+//     let str = '';
+//     let result = 0;
+//     for (let i = 1; i <= summary; i++) {
+//         str += i;
+//         if (str.length === summary) {
+//             result = i;
+//             break;
+//         }
+//     }
+//     return result;
+// }
+
+// console.log(amountOfPages(17));
+
+
+
+// function isPangram(string) {
+//     let abc = 'abcdefghijklmnopqrstuvwxyz';
+//     let str = string.toLowerCase().replaceAll(' ', '');
+//     str = new Set(str);
+//     for (let i = 0; i < abc.length; i++) {
+//         if(str.has(abc[i])){
+//             return true;
+//         }else {
+//             return false;
+//         }
+//     }
+// }
+
+// function isPangram(string){
+//     string = string.toLowerCase();
+//     return "abcdefghijklmnopqrstuvwxyz".split("").every(function(x){
+//       return string.indexOf(x) !== -1;
+//     });
+// }
+
+// console.log(isPangram('The quick brown fox jumps over the lazy dog'));
+// console.log(isPangram('The quick brown fox jumps over the lazy dog'));
+// console.log(isPangram('aBcdefg hijKlmnopqr stuVwxyz'));
+// console.log(isPangram('Hello world'));
+// isPangram('The quick brown fox jumps over the lazy dog');
+
+// let abc = 'abcdefghijklmnopqrstuvwxyz';
+// let abc2 = 'abcdefghijklmnopqrstuvwxyz';
+// // let str = new Set(abc);
+// // let str2 = new Set(abc);
+// // console.log(abc === abc2);
+
+// let a = '123';
+// let b = '321';
+// console.log(a === b);
