2dab45d142ae2ac8d144b8b0de87a4345c4c3058
Kerim <kkerimow123@gmail.com>
2023-10-28 03:57:54 +0300

    learned about call apply bind

--- practice_74/script.js
+++ practice_74/script.js
@@ -1,3 +1,15 @@
 'use strict';
 
 const box = document.querySelector('.box');
+
+const observer = new MutationObserver(mutationRecords => {
+    console.log(mutationRecords);
+});
+
+observer.observe(box, {
+    childList: true
+});
+
+// PerformanceObserver
+// ResizeObserver
+// IntersectionObserver
