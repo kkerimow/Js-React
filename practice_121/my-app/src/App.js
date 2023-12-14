639fc349c848f156db3b922c593aefc30706e109
Kerim <kkerimow123@gmail.com>
2023-12-14 23:50:27 +0300

    'components'

--- practice_121/my-app/src/App.js
+++ practice_121/my-app/src/App.js
@@ -1,18 +1,44 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
+
+const Header = () => {
+    return <h1>Hello World</h1>;
+}
+
+// const Field = () => {
+//     const holder = 'Enter here';
+//     const styledField = {
+//         width: '300px'
+//     };
+//     return <input type="text" placeholder={holder} style={styledField}/>;
+// }
+
+class Field extends Component {
+    render() {
+        const holder = 'Enter here';
+        const styledField = {
+            width: '300px'
+        };
+
+        return <input type="text" placeholder={holder} style={styledField}/>;
+    }
+}
+
+function Btn() {
+    const text = 'Log in';
+    const logged = true;
+    return <button>{logged? 'Enter': text}</button>
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-
-      </header>
+      <Header/>
+      <Field/>
+      <Btn/>
     </div>
   );
 }
 
+export {Header};
 export default App;
