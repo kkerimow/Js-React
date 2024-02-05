b967cbed82d1676e824a09cbeba1250748626298
Kerim <kkerimow123@gmail.com>
2024-02-06 00:17:24 +0300

    counter with react redux

--- practice_190/redux/src/index.js
+++ practice_190/redux/src/index.js
@@ -2,69 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {createStore} from 'redux';
 // import { legacy_createStore as createStore} from 'redux'
+import reducer from './reducer';
+import { Provider } from 'react-redux';
 
-
-const initialState = {value: 0};
-
-const reducer = (state=initialState, action) => {
-    switch (action.type) {
-        case 'INC':
-            return {
-                ...state,
-                value: state.value + 1
-            };
-        case 'DEC':
-            return {
-                ...state,
-                value: state.value - 1
-            };
-        case 'RND':
-            return {
-                ...state,
-                value: state.value * action.payload
-            };
-        default:
-            return state;
-    }
-
-}
+import App from './components/App';
 
 const store = createStore(reducer);
 
-const update = () => {
-    document.getElementById('counter').textContent = store.getState().value;
-};
-
-store.subscribe(update);
-
-const inc = () => ({type: 'INC'});
-const dec = () => ({type: 'DEC'});
-const rnd = (value) => ({type: 'RND', payload: value});
-
-document.getElementById('inc').addEventListener('click', () => {
-    store.dispatch(inc());
-})
-
-document.getElementById('dec').addEventListener('click', () => {
-    store.dispatch(dec());
-})
-
-document.getElementById('rnd').addEventListener('click', () => {
-    const value = Math.floor(Math.random() * 10);
-    store.dispatch(rnd(value));
-})
-
-
-// let state = reducer(initialState, {type: 'INC'});
-// state = reducer(state, {type: 'INC'});
-// state = reducer(state, {type: 'INC'});
-// state = reducer(state, {type: 'INC'});
-// console.log(state);
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <>
-    {/* <App /> */}
-  </>
+    <>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </>
 );
-
